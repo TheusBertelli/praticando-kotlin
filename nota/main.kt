a3e9a68ff40b02e9f8640b9602700c91db286495
Matheus Bertelli <matheusbertelli06@gmail.com>
2026-09-04 17:14:25 -0300

    adicionando o código

--- nota/main.kt
+++ nota/main.kt
@@ -1,1 +1,12 @@
-
+fun main(){
+    val nota = 75
+    
+    val conceito = when (nota){
+        in 70..79 -> "Nota boa"
+        in 80..89 -> "Nota muito boa"
+        in 90..100 -> "Gabaritou"
+        else -> "reprovado"
+        }
+        println("sua nota é $nota que é equivalenta á $conceito")
+}
+ 
